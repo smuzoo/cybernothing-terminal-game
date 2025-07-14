df7203edab5fc710f7b5905bff4392c185d63baf
smuzoo <lalun.75@gmail.com>
2025-07-14 04:47:11 +0300

    rebuild all

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/cybernothing-terminal-game/',
+  base: '/cybernothing-terminal-game/',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
   },
+  publicDir: 'public'
 });
